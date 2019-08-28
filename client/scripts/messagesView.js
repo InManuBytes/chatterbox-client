58dc8333b09c1ce3d634d23ffbf99749d09b0244
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-27 23:50:24 -0400

    [wip] Start on rendering messages with _.template function

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,15 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    // how does initialize works with render?
   },
 
   render: function() {
+    var i, html = "";
+    for (i = 0; i < data.length; i++) {
+      html += MessageView.render(data.???[i]); //what are we passing in?
+    }
+    $("#chats").append(html);
   }
 
 };
